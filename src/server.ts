7f4454a567c5bc2204f8b54c6379f3cba305dce4
Júlio César <julio.souzam@gmail.com>
2020-04-13 11:08:03 -0300

    struct project

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'Rocketseat' }));
+
+app.listen(3333, () => console.log('Server started on port 3333'));
